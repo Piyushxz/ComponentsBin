0fd7dbb62d4874d4781ced76ba152567e75cfb4b
Piyushxz <140832863+Piyushxz@users.noreply.github.com>
2025-04-12 16:31:54 +0530

    updated drawer

--- app/drawer/page.tsx
+++ app/drawer/page.tsx
@@ -1,11 +1,26 @@
 "use client";
 
-import { useState } from "react";
+import { DefaultView, Key, Phrase, RemoveWallet } from "@/components/DrawerComponents";
+import { useMemo, useState } from "react";
 import { Drawer } from 'vaul';
 
 export default function FamilyDrawer() {
   const [isOpen, setIsOpen] = useState(false);
 
+  const [view,setView] = useState("default")
+
+  const content = useMemo(() => {
+    switch (view) {
+      case "default":
+        return <DefaultView setView={setView} />;
+      case "remove":
+        return <RemoveWallet setView={setView} />;
+      case "phrase":
+        return <Phrase setView={setView} />;
+      case "key":
+        return <Key setView={setView} />;
+    }
+  }, [view]);
   return (
     <>
       <button
@@ -14,15 +29,24 @@ export default function FamilyDrawer() {
       >
         Try it out
       </button>
-      {isOpen ? (
+     
         <>
-          <div
+        <Drawer.Root open={isOpen} onOpenChange={setIsOpen}>
+            <Drawer.Portal>
+          <Drawer.Overlay
             className="fixed inset-0 z-10 bg-black/30"
             onClick={() => setIsOpen(false)}
           />
-          <div className="fixed inset-x-4 bottom-4 z-10 mx-auto h-64 max-w-[361px] overflow-hidden rounded-[36px] bg-[#FEFFFE] outline-none md:mx-auto md:w-full"></div>
+          
+          <Drawer.Content  className="fixed inset-x-4 bottom-4 z-10 mx-auto p-4 max-w-[361px] overflow-hidden rounded-[36px] bg-[#FEFFFE] outline-none md:mx-auto md:w-full">
+            {content}
+          </Drawer.Content>
+          </Drawer.Portal>
+          </Drawer.Root>
+
         </>
-      ) : null}
+    
+     
     </>
   );
 }
