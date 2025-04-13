69abac5a3464d30808f42537a0335da95d7e6a97
Piyushxz <140832863+Piyushxz@users.noreply.github.com>
2025-04-13 10:55:38 +0530

    improvements

--- app/drawer/page.tsx
+++ app/drawer/page.tsx
@@ -1,13 +1,19 @@
 "use client";
 
 import { DefaultView, Key, Phrase, RemoveWallet } from "@/components/DrawerComponents";
-import { useMemo, useState } from "react";
+import { useMemo, useRef, useState } from "react";
+import useMeasure from "react-use-measure";
+import { motion, AnimatePresence } from "framer-motion";
 import { Drawer } from 'vaul';
+import { CircleX } from "lucide-react";
 
 export default function FamilyDrawer() {
   const [isOpen, setIsOpen] = useState(false);
 
   const [view,setView] = useState("default")
+  const [elementRef, bounds] = useMeasure();
+  const previousHeightRef = useRef();
+
 
   const content = useMemo(() => {
     switch (view) {
@@ -21,6 +27,27 @@ export default function FamilyDrawer() {
         return <Key setView={setView} />;
     }
   }, [view]);
+  const opacityDuration = useMemo(() => {
+    const MIN_DURATION = 0.15;
+    const MAX_DURATION = 0.27;
+ 
+    if (!previousHeightRef.current) {
+      previousHeightRef.current = bounds.height;
+      return MIN_DURATION;
+    }
+ 
+    const heightDifference = Math.abs(
+      bounds.height - previousHeightRef.current,
+    );
+    previousHeightRef.current = bounds.height;
+ 
+    const duration = Math.min(
+      Math.max(heightDifference / 500, MIN_DURATION),
+      MAX_DURATION,
+    );
+ 
+    return duration;
+  }, [bounds.height]);
   return (
     <>
       <button
@@ -38,8 +65,46 @@ export default function FamilyDrawer() {
             onClick={() => setIsOpen(false)}
           />
           
-          <Drawer.Content  className="fixed inset-x-4 bottom-4 z-10 mx-auto p-4 max-w-[361px] overflow-hidden rounded-[36px] bg-[#FEFFFE] outline-none md:mx-auto md:w-full">
-            {content}
+          <Drawer.Content
+            asChild
+            className="fixed inset-x-4 bottom-4 z-10 mx-auto max-w-[361px] overflow-hidden rounded-[36px] bg-[#FEFFFE] outline-none md:mx-auto md:w-full"
+          >
+            <motion.div animate={{ height: bounds.height ,
+                  transition: {
+                    duration: 0.27,
+                    ease: [0.25, 1, 0.5, 1],
+                  },
+            }}
+            
+            >
+              <Drawer.Close asChild>
+                <button
+                  data-vaul-no-drag=""
+                  className="absolute right-8 top-7 z-10 flex h-8 w-8 items-center justify-center rounded-full bg-[#F7F8F9] text-[#949595] transition-transform focus:scale-95 focus-visible:shadow-focus-ring-button active:scale-75"
+                >
+                  <CircleX />
+                </button>
+              </Drawer.Close>
+              <div   ref={elementRef} className="px-6 pb-6 pt-2.5 antialiased">
+              <AnimatePresence mode="popLayout" custom={view}>
+              <motion.div 
+              initial={{opacity:0,scale:0.96}}
+              animate={{opacity:1,scale:1}}
+              exit={{opacity:0,scale:0.96}}
+              transition={{
+                duration: opacityDuration,
+                ease: [0.26, 0.08, 0.25, 1],
+              }}
+              key={view}
+             >
+                {content}
+              </motion.div>
+              </AnimatePresence>
+              </div>
+
+
+ 
+            </motion.div>
           </Drawer.Content>
           </Drawer.Portal>
           </Drawer.Root>
