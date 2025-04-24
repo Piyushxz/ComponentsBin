81ece4cfd13ca4bcfdf7682e7c4650fe1a91c36a
Piyushxz <140832863+Piyushxz@users.noreply.github.com>
2025-04-24 21:32:31 +0530

    latest

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import { Tabs } from "@/components/TabsTransition/Tabs";
 
 export default function Home() {
   return (
     <div className="w-screen h-[100vh] flex justify-center items-center ">
+      <Tabs/>
     </div>
   );
 }
