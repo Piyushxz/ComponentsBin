b83b76222347ee398969ad3b141863c0233827df
Piyushxz <140832863+Piyushxz@users.noreply.github.com>
2025-04-26 21:57:25 +0530

    latest

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
+import SharedLayout from "@/components/iOSCard/iosStore";
 import { Tabs } from "@/components/TabsTransition/Tabs";
 
 export default function Home() {
   return (
-    <div className="w-screen h-[100vh] flex justify-center items-center ">
-      <Tabs/>
-    </div>
+    <SharedLayout/>
   );
 }
