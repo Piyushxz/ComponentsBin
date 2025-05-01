da45dfe567419fd254a2abf7e1ca7a85dadce147
Piyushxz <140832863+Piyushxz@users.noreply.github.com>
2025-05-01 12:36:41 +0530

    latest

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 import SharedLayout from "@/components/iOSCard/iosStore";
 import { Tabs } from "@/components/TabsTransition/Tabs";
+import { TrashAnimation } from "@/components/TrashComponent/Component";
 
 export default function Home() {
   return (
-    <SharedLayout/>
+   <TrashAnimation/>
   );
 }
